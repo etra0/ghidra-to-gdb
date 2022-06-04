10b14b6366352a9bf5169733baa1c8be35c2eb72
Sebastián Aedo <sebastian.aedo29@gmail.com>
2022-06-03 22:19:07 -0400

    chore: pylint suggestions.

--- ghidra_to_gdb.py
+++ ghidra_to_gdb.py
@@ -1,3 +1,9 @@
+"""
+Small utility to generate debugging symbols from a ghidra-exported header.
+This is useful to load them to GDB and then cast raw pointers to the structs to
+have a pretty print!
+"""
+
 from subprocess import Popen, PIPE
 import re
 import sys
@@ -7,8 +13,8 @@ STRUCT_RE = re.compile(r"struct (\w+)")
 
 def main(filename: str, extra_args: str, output_name: str):
     definitions = None
-    with open(filename, 'r') as f:
-        definitions = f.read()
+    with open(filename, 'r', encoding='utf-8') as header:
+        definitions = header.read()
 
     # By default, ghidra doesn't export the `pointer` type, and some
     # definitions use the pointer32 too.
@@ -24,22 +30,25 @@ def main(filename: str, extra_args: str, output_name: str):
     for struct in finds:
         definitions += f"struct {struct} dummy_{struct};\n"
 
-    args = ['gcc', '-g', '-c', '-xc', f'-o{output_name}']
+    popen_args = ['gcc', '-g', '-c', '-xc', f'-o{output_name}']
     if extra_args is not None:
-        args += extra_args.split(',')
+        popen_args += extra_args.split(',')
 
     # We compile from stdin.
-    args.append('-')
+    popen_args.append('-')
 
-    p = Popen(args, stdout=PIPE, stdin=PIPE, stderr=PIPE)
-    result = p.communicate(input=definitions.encode())
-    out, err = result
+    returncode = None
+    out, err = (None, None)
+    with Popen(popen_args, stdout=PIPE, stdin=PIPE, stderr=PIPE) as proc:
+        result = proc.communicate(input=definitions.encode())
+        out, err = result
+        returncode = proc.returncode
 
-    if p.returncode:
+    if proc.returncode:
         print(f"""Something went wrong while running:\
-                \n > {" ".join(args)} \
+                \n > {" ".join(popen_args)} \
                 \n{err.decode()}""")
-        sys.exit(p.returncode)
+        sys.exit(returncode)
 
     if len(out) != 0:
         print(f"Compiled with some warnings:\n\n{out.decode()}")
@@ -49,7 +58,10 @@ def main(filename: str, extra_args: str, output_name: str):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate obj file with symbol information")
     parser.add_argument('header', help="Path of the header file")
-    parser.add_argument('--gcc_args', help="Extra arguments for GCC, separated by a comma (,)")
-    parser.add_argument('--output', help="Name of the output file, default is symbols.o", default="symbols.o")
+    parser.add_argument('--gcc_args',
+            help="Extra arguments for GCC, separated by a comma (,)")
+    parser.add_argument('--output',
+            help="Name of the output file, default is symbols.o",
+            default="symbols.o")
     args = parser.parse_args()
     main(args.header, args.gcc_args, args.output)
